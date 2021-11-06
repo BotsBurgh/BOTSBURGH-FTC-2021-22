1ea1bd751c6c8028410585043c6c0fc1481aecc3
BD103 <59022059+BD103@users.noreply.github.com>
2021-11-06 17:17:56 -0400

    feat: create base v2 api

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/API/v2/Naming.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.API.v2;
+
+/**
+ * Constants for getting <code>hardwareMap</code> items
+ */
+public class Naming {
+    public static final String FRONT_LEFT = "frontLeft";
+    public static final String FRONT_RIGHT = "frontRight";
+    public static final String BACK_LEFT = "backLeft";
+    public static final String BACK_RIGHT = "backRight";
+}
