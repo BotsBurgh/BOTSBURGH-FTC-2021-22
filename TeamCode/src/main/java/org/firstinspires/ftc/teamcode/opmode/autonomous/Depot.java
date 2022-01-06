3e254747a7ed25bd4308ef44ffee09611b59e97b
BD103 <59022059+BD103@users.noreply.github.com>
2022-01-05 19:22:04 -0500

    feat: make depot use merged robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/Depot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/Depot.java
@@ -5,19 +5,28 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.api.InitRobot;
 import org.firstinspires.ftc.teamcode.api.Movement;
+import org.firstinspires.ftc.teamcode.api.Robot;
 import org.firstinspires.ftc.teamcode.api.config.Naming;
 
 @Autonomous(name = "Depot", group = Naming.OPMODE_GROUP_COMP)
 public class Depot extends LinearOpMode {
     @Override
     public void runOpMode() {
-        InitRobot.init(this);
+        Robot robot = new Robot(this);
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+
         waitForStart();
+
         telemetry.addData("Status", "Running");
-        Movement.move1x4(1);
+        telemetry.update();
+
+        robot.powerWheels(1);
         sleep(2000);
-        Movement.move1x4(0);
+        robot.powerWheels(0);
+
+        telemetry.addData("Status", "Finished");
+        telemetry.update();
     }
 }
