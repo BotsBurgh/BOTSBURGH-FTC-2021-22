c81548f503c22982e98d5fdcad73e031c07e93f4
BD103 <59022059+BD103@users.noreply.github.com>
2022-01-15 15:53:17 -0500

    feat: make robot move left before forward

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/MoveForward2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/MoveForward2.java
@@ -2,12 +2,15 @@ package org.firstinspires.ftc.teamcode.opmode.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.api.Robot;
 import org.firstinspires.ftc.teamcode.api.config.Naming;
 
 @Autonomous(name = "Move Forward 2", group = Naming.OPMODE_GROUP_COMP)
 public class MoveForward2 extends LinearOpMode {
+    private ElapsedTime runtime;
+
     @Override
     public void runOpMode() {
         Robot robot = new Robot(this);
@@ -20,9 +23,25 @@ public class MoveForward2 extends LinearOpMode {
         telemetry.addData("Status", "Running");
         telemetry.update();
 
-        robot.powerWheels(1);
-        sleep(2000);
+        // Move left a little bit
+        runtime = new ElapsedTime();
+
+        while (runtime.seconds() < 0.5 && opModeIsActive()) {
+            robot.powerWheels(-0.5, 0.5, 0.5, -0.5);
+        }
+
+        sleep(500);
+
+        // Move forward
+        runtime = new ElapsedTime();
+
+        while (runtime.seconds() < 2 && opModeIsActive()) {
+            robot.powerWheels(1);
+            robot.powerStepWheels(1);
+        }
+
         robot.powerWheels(0);
+        robot.powerStepWheels(0);
 
         telemetry.addData("Status", "Finished");
         telemetry.update();
