f7d6e21deb67bd2be6fd7ce50bec618ff7cf9b99
BD103 <59022059+BD103@users.noreply.github.com>
2022-01-22 16:00:21 -0500

    refactor: rename DistanceAuto to GetDistance
    It now no longer moves the robot, but just prints the distance to telementry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/GetDistance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/GetDistance.java
@@ -8,32 +8,31 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.api.Robot;
 import org.firstinspires.ftc.teamcode.api.config.Naming;
 
-@Autonomous(name = "Distance Auto", group= Naming.OPMODE_GROUP_COMP)
-public class DistanceAuto extends LinearOpMode {
+@Autonomous(name = "Get Distance", group= Naming.OPMODE_GROUP_UTIL)
+public class GetDistance extends LinearOpMode {
     @Override
     public void runOpMode() {
         Robot robot = new Robot(this);
         DistanceSensor sensorL = hardwareMap.get(DistanceSensor.class, "distance_left");
         DistanceSensor sensorR = hardwareMap.get(DistanceSensor.class, "distance_right");
 
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
         waitForStart();
 
-        boolean running = true;
-        double distance = 100;
+        telemetry.addData("Status", "Running");
+        telemetry.update();
 
-        while (distance > 50) {
+        while (opModeIsActive()) {
             double distanceL = sensorL.getDistance(DistanceUnit.CM);
             double distanceR = sensorR.getDistance(DistanceUnit.CM);
             distance = (distanceL + distanceR) / 2;
-
-            robot.powerWheels(0.5);
 
             telemetry.addData("Average Distance", distance);
             telemetry.addData("Left Distance", distanceL);
             telemetry.addData("Right Distance", distanceR);
             telemetry.update();
         }
-
-        robot.powerDuck(0);
     }
 }
