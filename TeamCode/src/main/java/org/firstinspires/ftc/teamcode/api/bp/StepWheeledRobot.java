ed9f174415027b94c0230cde8e272b7a4c782a3c
BD103 <59022059+BD103@users.noreply.github.com>
2022-02-12 17:30:22 -0500

    breaking: deprecate stepwheeledrobot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/bp/StepWheeledRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/bp/StepWheeledRobot.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.api.bp;
 
+@Deprecated
 public interface StepWheeledRobot {
     default void powerStepWheels(double leftPower, double rightPower) {}
     default void powerStepWheels(double power) {this.powerStepWheels(power, power);};
