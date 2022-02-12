0867e5b36c11430cccaf06d1daa2a5ca4fea0c5c
BD103 <59022059+BD103@users.noreply.github.com>
2022-02-12 16:53:50 -0500

    refactor: get rid of more whitespace

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/config/Naming.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/config/Naming.java
@@ -6,25 +6,25 @@ package org.firstinspires.ftc.teamcode.api.config;
  */
 public class Naming {
     // Name configuration
-    public static final String MOTOR_FL                           = "frontLeft";
-    public static final String MOTOR_BL                           = "backLeft";
-    public static final String MOTOR_FR                           = "frontRight";
-    public static final String MOTOR_BR                           = "backRight";
+    public static final String MOTOR_FL = "frontLeft";
+    public static final String MOTOR_BL = "backLeft";
+    public static final String MOTOR_FR = "frontRight";
+    public static final String MOTOR_BR = "backRight";
 
-    public static final String MOTOR_DUCK                         = "duck";
+    public static final String MOTOR_DUCK = "duck";
 
-    public static final String SERVO_ARM_LEFT                     = "armBaseLeft";
-    public static final String SERVO_ARM_RIGHT                    = "armBaseRight";
+    public static final String SERVO_ARM_LEFT = "armBaseLeft";
+    public static final String SERVO_ARM_RIGHT = "armBaseRight";
 
-    public static final String SERVO_CLAW_LEFT                    = "clawLeft";
-    public static final String SERVO_CLAW_RIGHT                   = "clawRight";
+    public static final String SERVO_CLAW_LEFT = "clawLeft";
+    public static final String SERVO_CLAW_RIGHT = "clawRight";
 
-    public static final String COLOR_SENSOR_PARK                  = "parkSensor";
+    public static final String COLOR_SENSOR_PARK = "parkSensor";
 
-    public static final String GYRO_0                             = "imu";
-    public static final String GYRO_1                             = "imu 1";
+    public static final String GYRO_0 = "imu";
+    public static final String GYRO_1 = "imu 1";
 
-    public static final String WEBCAM_0                           = "Webcam 1";
+    public static final String WEBCAM_0 = "Webcam 1";
 
     // OpMode group constants
     public static final String OPMODE_GROUP_COMP = "00-Competition"; // OpModes we would run during a competition
@@ -32,4 +32,3 @@ public class Naming {
     public static final String OPMODE_GROUP_DEMO = "60-Demos"; // Demo OpModes that are useless outside of showing off
     public static final String OPMODE_GROUP_TEST = "99-Test"; // Testing OpModes that move the robot
 }
-
