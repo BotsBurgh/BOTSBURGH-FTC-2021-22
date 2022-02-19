f846f42f8a87dca500089462ef31b37459de7f0d
BD103 <59022059+BD103@users.noreply.github.com>
2022-02-19 17:01:20 -0500

    feat: turn get distance into teleop
    Now it can run forever

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/GetDistance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/autonomous/GetDistance.java
@@ -1,14 +1,12 @@
 package org.firstinspires.ftc.teamcode.opmode.autonomous;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.api.Robot;
 import org.firstinspires.ftc.teamcode.api.config.Naming;
 
-@Autonomous(name = "Get Distance", group= Naming.OPMODE_GROUP_UTIL)
+@TeleOp(name = "Get Distance", group= Naming.OPMODE_GROUP_UTIL)
 public class GetDistance extends LinearOpMode {
     @Override
     public void runOpMode() {
