22a44e8a6139af498cf6c83f491731ab4e651a59
BD103 <59022059+BD103@users.noreply.github.com>
2022-03-19 17:28:23 -0400

    BREAKING: delete StepWheeledRobot interface

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/bp/StepWheeledRobot.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.api.bp;
-
-@Deprecated
-public interface StepWheeledRobot {
-    default void powerStepWheels(double leftPower, double rightPower) {}
-    default void powerStepWheels(double power) {this.powerStepWheels(power, power);};
-}
